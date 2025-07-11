ad1d63150f719bd4905921e542c290c1913cebd8
boyninja15 <flappyfloorg@gmail.com>
2025-07-11 13:46:24 -0500

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m nuitka --clang --standalone --onefile --output-filename=floor-browse src/main.py
+python3 -m nuitka --clang --standalone --onefile --output-filename=floor-browse --enable-plugin=tk-inter --include-package=lupa src/main.py
